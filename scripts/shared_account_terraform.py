bfb04f40262c8f17316c56f27a1dea69524295e6
Brett Minnie <github@bdb-studios.co.uk>
2021-12-30 12:15:33 +0000

    Fixed importer script

--- scripts/shared_account_terraform.py
+++ scripts/shared_account_terraform.py
@@ -29,6 +29,7 @@ class TerraformRunner(object):
     _state_files = {
         "infra": "tfstatefile",
         "infra_ci": "ci/tfstatefile",
+        "infra_ci_sonarqube": "sonarqube/tfstatefile",
         "users": "users/tfstatefile"
     }
 
