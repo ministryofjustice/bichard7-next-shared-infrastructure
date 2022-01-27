c0c884f4b3e61c15c63621cc6cdd813a0709aeba
Brett Minnie <github@bdb-studios.co.uk>
2022-01-27 16:33:12 +0000

    Moved user management to ansible

--- terraform/shared_account_pathtolive_users/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-
-  backend "s3" {
-    bucket         = "cjse-bichard7-default-pathtolive-bootstrap-tfstate"
-    key            = "users/tfstatefile"
-    dynamodb_table = "cjse-bichard7-default-pathtolive-bootstrap-tfstate-lock"
-  }
-}
--- terraform/shared_account_sandbox_users/terraform.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-
-  backend "s3" {
-    bucket = "cjse-bichard7-default-sharedaccount-sandbox-bootstrap-tfstate"
-    key    = "users/tfstatefile"
-  }
-}
