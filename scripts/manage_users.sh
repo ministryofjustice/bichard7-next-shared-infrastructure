22174807241192aee3a727ddad558c76602dfeda
Brett Minnie <github@bdb-studios.co.uk>
2022-01-28 09:07:28 +0000

    Pinned ansible and boto versions

--- scripts/manage_users.sh
+++ scripts/manage_users.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+set -e
+
 pip3 install virtualenv
 python3 -m venv .venv
 source .venv/bin/activate
-pip3 install ansible boto3
+pip3 install ansible==5.2.0 boto3==1.20.45
 ansible-galaxy collection install community.aws
 cd ansible
 ENVIRONMENT=${WORKSPACE:-sandbox} ansible-playbook playbook.yml
