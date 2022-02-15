fb642aba1691e3d2194150f8ad0ca15e68786e66
Brett Minnie <github@bdb-studios.co.uk>
2022-02-15 15:15:47 +0000

    Removed unneeded providers

--- terraform/shared_account_pathtolive_infra_ci_monitoring/providers.tf
+++ terraform/shared_account_pathtolive_infra_ci_monitoring/providers.tf
@@ -1,6 +1,1 @@
 provider "aws" {}
-
-provider "grafana" {
-  url  = "https://${module.codebuild_monitoring.grafana_external_fqdn}"
-  auth = "${module.codebuild_monitoring.grafana_admin_user_name}:${module.codebuild_monitoring.grafana_admin_password}"
-}
--- terraform/shared_account_pathtolive_infra_ci_monitoring/versions.tf
+++ terraform/shared_account_pathtolive_infra_ci_monitoring/versions.tf
@@ -4,17 +4,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "= 3.72.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = "= 2.2.0"
-    }
     local = {
       source  = "hashicorp/local"
       version = "= 2.0.0"
-    }
-    grafana = {
-      source  = "grafana/grafana"
-      version = "1.19.0"
     }
   }
   required_version = ">= 0.13"
