729b12199b1fd2cbd379d7fd8fde08ebc1f78f2b
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2022-06-28 14:00:59 +0100

    fix: 🐛 ansible was deleting the last user in the array (#69)

--- scripts/manage_users.sh
+++ scripts/manage_users.sh
@@ -5,7 +5,7 @@ set -e
 pip3 install virtualenv
 python3 -m venv .venv
 source .venv/bin/activate
-pip3 install ansible==5.2.0 boto3==1.20.45
+pip3 install ansible==6.0.0 boto3
 ansible-galaxy collection install community.aws
 cd ansible
 ENVIRONMENT=${WORKSPACE:-sandbox} ansible-playbook playbook.yml
