a44acea28239824d6ab0193900f0b6c9cafdfba0
jaskaran <jaskaran.sarkaria@googlemail.com>
2022-07-15 12:46:47 +0100

    fix: 🐛 cidr block 0.0.0.0 & perms to desc repos for ecr alarm

--- terraform/shared_account_pathtolive_infra_ci_monitoring/ecr_alarms.tf
+++ terraform/shared_account_pathtolive_infra_ci_monitoring/ecr_alarms.tf
@@ -36,6 +36,7 @@ resource "aws_iam_policy" "lambda_allow_to_list_images" {
         Effect = "Allow"
         Action = [
           "ecr:ListImages",
+          "ecr:DescribeRepositories"
         ]
         Resource = [
           "*"
@@ -56,24 +57,25 @@ resource "aws_iam_role" "ecr_repo_images" {
   tags                = module.label.tags
 }
 
-resource "aws_security_group" "slack_lambda_access_to_vpc" {
-  name        = "slack-lambda-to-vpc"
-  description = "A Security Group for the resource to VPC"
+resource "aws_security_group" "slack_lambda_access_to_internet" {
+  name        = "slack-lambda-to-internet"
+  description = "A Security Group for the resource to internet"
   vpc_id      = data.terraform_remote_state.shared_infra_ci.outputs.codebuild_vpc_id
 
-  tags = merge(module.label.tags, { Name = "slack-lambda-to-vpc" })
+  tags = merge(module.label.tags, { Name = "slack-lambda-to-internet" })
 }
 
-resource "aws_security_group_rule" "resource_to_vpc_egress" {
-  description = "Allow traffic from the resource to the VPC"
+# tfsec:ignore:aws-vpc-no-public-egress-sgr
+resource "aws_security_group_rule" "resource_to_internet_egress" {
+  description = "Allow traffic from the resource to the internet"
 
-  security_group_id = aws_security_group.slack_lambda_access_to_vpc.id
+  security_group_id = aws_security_group.slack_lambda_access_to_internet.id
   type              = "egress"
   protocol          = "tcp"
   from_port         = 443
   to_port           = 443
 
-  cidr_blocks = [data.terraform_remote_state.shared_infra_ci.outputs.codebuild_cidr_block]
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
 # tfsec:ignore:aws-lambda-enable-tracing
@@ -87,7 +89,7 @@ resource "aws_lambda_function" "query_images_fn" {
 
   vpc_config {
     subnet_ids         = data.terraform_remote_state.shared_infra_ci.outputs.codebuild_subnet_ids
-    security_group_ids = [aws_security_group.slack_lambda_access_to_vpc.id]
+    security_group_ids = [aws_security_group.slack_lambda_access_to_internet.id]
   }
 
   role        = aws_iam_role.ecr_repo_images.arn
