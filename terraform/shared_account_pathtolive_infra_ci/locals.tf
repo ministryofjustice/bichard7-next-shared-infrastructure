67aea7a87a77630803311323ae1edf88aff7c2ce
Ian King <ian.antking@gmail.com>
2023-02-03 13:19:09 +0000

    deploy e2e & prepod vpn rotation script

--- terraform/shared_account_pathtolive_infra_ci/locals.tf
+++ terraform/shared_account_pathtolive_infra_ci/locals.tf
@@ -67,12 +67,12 @@ locals {
       deploy_account_name = "integration_next"
     },
     {
-      id           = data.aws_caller_identity.preprod.account_id
-      target       = "preprod"
-      role_arn     = data.terraform_remote_state.shared_infra.outputs.preprod_ci_arn
-      workspace    = "pre-prod"
-      account_name = "preprod"
-      deploy_name  = "preprod"
+      id                  = data.aws_caller_identity.preprod.account_id
+      target              = "preprod"
+      role_arn            = data.terraform_remote_state.shared_infra.outputs.preprod_ci_arn
+      workspace           = "pre-prod"
+      account_name        = "preprod"
+      deploy_name         = "preprod"
       deploy_account_name = "q_solution"
     },
     # {
