bc5f63ddd53ce086f2c20700c84938902eddbfdb
Ian King <ian.antking@gmail.com>
2023-03-07 16:01:03 +0000

    use nuke user to test job

--- terraform/modules/shared_cd_common_jobs/access_layer.tf
+++ terraform/modules/shared_cd_common_jobs/access_layer.tf
@@ -1,12 +1,16 @@
 module "apply_access_layer" {
-  source                 = "../codebuild_job"
-  build_description      = "Apply the shared account infra layer on a schedule"
-  codepipeline_s3_bucket = var.codebuild_s3_bucket
-  name                   = "apply-access-layer"
-  sns_kms_key_arn        = var.notifications_kms_key_arn
-  sns_notification_arn   = var.sns_notifications_arn
-  repository_name        = "bichard7-next-shared-infrastructure"
-  buildspec_file         = "applyci-access.yml"
+  source                         = "../codebuild_job"
+  build_description              = "Apply the shared account infra layer on a schedule"
+  codepipeline_s3_bucket         = var.codebuild_s3_bucket
+  name                           = "apply-access-layer"
+  sns_kms_key_arn                = var.notifications_kms_key_arn
+  sns_notification_arn           = var.sns_notifications_arn
+  repository_name                = "bichard7-next-shared-infrastructure"
+  buildspec_file                 = "applyci-access.yml"
+  aws_access_key_id_ssm_path     = "/nuke/user/access_key_id"
+  aws_secret_access_key_ssm_path = "/nuke/user/secret_access_key"
+
+
 
   environment_variables = [
     {
