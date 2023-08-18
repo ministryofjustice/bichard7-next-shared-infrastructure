e6a1494709a01ecb351c876df6e497ccc6d2e520
Ian King <ian.antking@gmail.com>
2023-08-18 15:09:41 +0100

    add cd job for nginx_nodejs_2023_supervisored image

---
+++ terraform/modules/shared_cd_common_jobs/build_nginx_2023_base.tf
@@ -0,0 +1,24 @@
+module "build_nginx_nodejs_2023_supervisord_docker_image" {
+  source = "../codebuild_job"
+
+  name              = "build-nginx-nodejs-2023-supervisord-docker"
+  build_description = "Codebuild for Building Nginx and Supervisord On NodeJS 16 Base With amazonlinux:2023"
+  repository_name   = "bichard7-next-infrastructure-docker-images"
+  buildspec_file    = "./Nginx_NodeJS_2023_Supervisord/buildspec.yml"
+
+  codepipeline_s3_bucket = var.codebuild_s3_bucket
+  sns_notification_arn   = var.sns_notifications_arn
+  sns_kms_key_arn        = var.notifications_kms_key_arn
+  vpc_config             = var.vpc_config_block
+  environment_variables  = var.common_cd_vars
+  tags                   = var.tags
+}
+
+module "build_nginx_nodejs_supervisord_docker_image_schedule" {
+  source          = "../codebuild_schedule"
+  codebuild_arn   = module.build_nginx_nodejs_2023_supervisord_docker_image.pipeline_arn
+  name            = module.build_nginx_nodejs_2023_supervisord_docker_image.pipeline_name
+  cron_expression = "cron(0 4 ? * SUN *)"
+
+  tags = var.tags
+}
