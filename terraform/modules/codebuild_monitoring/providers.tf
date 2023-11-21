0a52d3cb3fb20534351eb97c349043e4ef85f7c7
Emad Karamad <emad.karamad@madetech.com>
2023-11-21 16:18:27 +0000

    Remove codebuild-monitoring module (#195)

--- terraform/modules/codebuild_monitoring/providers.tf
+++
@@ -1,4 +0,0 @@
-provider "grafana" {
-  url  = "https://${aws_route53_record.grafana_public_record.fqdn}"
-  auth = "${aws_ssm_parameter.grafana_admin_username.value}:${aws_ssm_parameter.grafana_admin_password.value}"
-}
--- terraform/shared_account_pathtolive_infra_ci_monitoring/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "grafana_url" {
-  description = "The url of our grafana server"
-  value       = module.codebuild_monitoring.grafana_external_fqdn
-}
