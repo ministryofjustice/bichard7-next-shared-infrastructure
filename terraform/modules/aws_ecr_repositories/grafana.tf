a9422ce53f93263528e462262db032c13afdd4a2
Emad Karamad <emad.karamad@madetech.com>
2023-11-22 09:54:17 +0000

    Remove grafana codebuild ecr repo and codebuild job

--- terraform/modules/aws_ecr_repositories/grafana.tf
+++ terraform/modules/aws_ecr_repositories/grafana.tf
@@ -19,25 +19,3 @@ resource "aws_ecr_lifecycle_policy" "grafana" {
   policy     = file("${path.module}/policies/application_image_ecr_lifecycle_policy.json")
   repository = aws_ecr_repository.grafana.name
 }
-
-# tfsec:ignore:aws-ecr-repository-customer-key
-resource "aws_ecr_repository" "grafana_codebuild" {
-  name                 = "grafana-codebuild"
-  image_tag_mutability = "IMMUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  tags = var.tags
-}
-
-resource "aws_ecr_repository_policy" "allow_codebuild_grafana_codebuild" {
-  policy     = data.template_file.shared_docker_image_policy.rendered
-  repository = aws_ecr_repository.grafana_codebuild.name
-}
-
-resource "aws_ecr_lifecycle_policy" "grafana_codebuild" {
-  policy     = file("${path.module}/policies/application_image_ecr_lifecycle_policy.json")
-  repository = aws_ecr_repository.grafana_codebuild.name
-}
