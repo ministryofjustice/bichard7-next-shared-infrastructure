df25473cdf6a4428e5d3be07a03ae2b39d1763b0
Emad Karamad <emad.karamad@madetech.com>
2023-11-22 13:43:47 +0000

    Update specific cb jobs to use ci-admin user

--- terraform/modules/shared_cd_common_jobs/cd_layer.tf
+++ terraform/modules/shared_cd_common_jobs/cd_layer.tf
@@ -1,12 +1,14 @@
 module "apply_codebuild_layer" {
-  source                 = "../codebuild_job"
-  build_description      = "Apply the shared account ci layer on a schedule"
-  codepipeline_s3_bucket = var.codebuild_s3_bucket
-  name                   = "apply-codebuild-layer"
-  sns_kms_key_arn        = var.notifications_kms_key_arn
-  sns_notification_arn   = var.sns_notifications_arn
-  repository_name        = "bichard7-next-shared-infrastructure"
-  buildspec_file         = "applyci-buildspec.yml"
+  source                         = "../codebuild_job"
+  build_description              = "Apply the shared account ci layer on a schedule"
+  codepipeline_s3_bucket         = var.codebuild_s3_bucket
+  name                           = "apply-codebuild-layer"
+  sns_kms_key_arn                = var.notifications_kms_key_arn
+  sns_notification_arn           = var.sns_notifications_arn
+  repository_name                = "bichard7-next-shared-infrastructure"
+  buildspec_file                 = "applyci-buildspec.yml"
+  aws_access_key_id_ssm_path     = "/ci-admin/user/access_key_id"
+  aws_secret_access_key_ssm_path = "/ci-admin/user/secret_access_key"
 
   environment_variables = [
     {
--- terraform/modules/shared_cd_common_jobs/manage_aws_users.tf
+++ terraform/modules/shared_cd_common_jobs/manage_aws_users.tf
@@ -1,12 +1,14 @@
 module "manage_aws_users" {
-  source                 = "../codebuild_job"
-  build_description      = "Run user management jobs on a schedule"
-  codepipeline_s3_bucket = var.codebuild_s3_bucket
-  name                   = "manage-aws-users"
-  sns_kms_key_arn        = var.notifications_kms_key_arn
-  sns_notification_arn   = var.sns_notifications_arn
-  repository_name        = "bichard7-next-shared-infrastructure"
-  buildspec_file         = "manage-users-buildspec.yml"
+  source                         = "../codebuild_job"
+  build_description              = "Run user management jobs on a schedule"
+  codepipeline_s3_bucket         = var.codebuild_s3_bucket
+  name                           = "manage-aws-users"
+  sns_kms_key_arn                = var.notifications_kms_key_arn
+  sns_notification_arn           = var.sns_notifications_arn
+  repository_name                = "bichard7-next-shared-infrastructure"
+  buildspec_file                 = "manage-users-buildspec.yml"
+  aws_access_key_id_ssm_path     = "/ci-admin/user/access_key_id"
+  aws_secret_access_key_ssm_path = "/ci-admin/user/secret_access_key"
 
   environment_variables = [
     {
