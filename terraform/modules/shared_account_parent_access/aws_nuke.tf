8a819ebe3d926662f79eaf84476b51e7d7c74d32
Emad Karamad <emad.karamad@madetech.com>
2024-03-11 10:07:19 +0000

    Refactor aws nuke policy template file variable

--- terraform/modules/shared_account_parent_access/aws_nuke.tf
+++ terraform/modules/shared_account_parent_access/aws_nuke.tf
@@ -1,7 +1,12 @@
 resource "aws_iam_policy" "allow_assume_aws_nuke_access_role" {
-  count  = (var.aws_nuke_access_arn != null) ? 1 : 0
-  name   = "Assume-AWS-Nuke-Access-on-${var.child_account_id}"
-  policy = local.allow_assume_aws_nuke_access_policy
+  count = (var.aws_nuke_access_arn != null) ? 1 : 0
+  name  = "Assume-AWS-Nuke-Access-on-${var.child_account_id}"
+  policy = templatefile(
+    "${path.module}/policies/allow_assume_aws_nuke_access.json.tpl",
+    {
+      aws_nuke_access_arn = var.aws_nuke_access_arn
+    }
+  )
 
   tags = var.tags
 }
