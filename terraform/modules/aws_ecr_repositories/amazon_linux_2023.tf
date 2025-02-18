e95e865db558364443180b08288f0784c1ca70e6
Emad Karamad <emad.karamad@madetech.com>
2025-02-18 10:57:46 +0000

    Add codebuild-2023-base ecr repository

--- terraform/modules/aws_ecr_repositories/amazon_linux_2023.tf
+++ terraform/modules/aws_ecr_repositories/amazon_linux_2023.tf
@@ -77,3 +77,25 @@ resource "aws_ecr_lifecycle_policy" "amazon_linux_2023_base" {
   policy     = file("${path.module}/policies/builder_image_ecr_lifecycle_policy.json")
   repository = aws_ecr_repository.amazon_linux_2023_base.name
 }
+
+# tfsec:ignore:aws-ecr-repository-customer-key
+resource "aws_ecr_repository" "codebuild_2023_base" {
+  name                 = "codebuild-2023-base"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = var.tags
+}
+
+resource "aws_ecr_lifecycle_policy" "codebuild_2023_base" {
+  policy     = file("${path.module}/policies/builder_image_ecr_lifecycle_policy.json")
+  repository = aws_ecr_repository.codebuild_2023_base.name
+}
+
+resource "aws_ecr_repository_policy" "allow_codebuild_2023_codebuild_base" {
+  policy     = file("${path.module}/templates/codebuild_image_policy.json")
+  repository = aws_ecr_repository.codebuild_2023_base.name
+}
