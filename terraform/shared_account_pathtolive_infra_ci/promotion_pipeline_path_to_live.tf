0cac073983b1a15fe36d4f42671994fed1e2d62e
joe-fol <joefolkard@protonmail.com>
2025-02-25 13:56:16 +0000

    Add leds test environment to pipeline

--- terraform/shared_account_pathtolive_infra_ci/promotion_pipeline_path_to_live.tf
+++ terraform/shared_account_pathtolive_infra_ci/promotion_pipeline_path_to_live.tf
@@ -417,6 +417,141 @@ resource "aws_codepipeline" "path_to_live" {
   }
 
   stage {
+    name = "deploy-and-test-leds-test-environment"
+    action {
+      category        = "Build"
+      name            = "fetch-and-update-leds-test-deploy-tags"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      run_order       = 1
+      version         = "1"
+      namespace       = "HASHES"
+      input_artifacts = ["infrastructure"]
+
+      configuration = {
+        ProjectName = module.update_environment_ssm_params.pipeline_name
+      }
+    }
+
+    action {
+      category  = "Build"
+      name      = "deploy-leds-test-environment"
+      owner     = "AWS"
+      provider  = "CodeBuild"
+      version   = "1"
+      run_order = 2
+
+      configuration = {
+        ProjectName   = module.deploy_e2e_test_terraform.pipeline_name
+        PrimarySource = "infrastructure"
+        EnvironmentVariables = jsonencode(
+          [
+            {
+              name  = "TF_VAR_override_deploy_tags"
+              value = "true"
+            },
+            {
+              name  = "TF_VAR_bichard_deploy_tag"
+              value = "#{HASHES.WAS_APPLICATION_IMAGE_HASH}"
+            },
+            {
+              name  = "TF_VAR_audit_logging_fn_hash"
+              value = "#{HASHES.AUDIT_LOGGING_COMMIT_HASH}"
+            },
+            {
+              name  = "TF_VAR_core_fn_hash"
+              value = "#{HASHES.CORE_COMMIT_HASH}"
+            },
+            {
+              name  = "TF_VAR_user_service_deploy_tag"
+              value = "#{HASHES.USER_SERVICE_IMAGE_HASH}"
+            },
+            {
+              name  = "TF_VAR_nginx_auth_proxy_deploy_tag"
+              value = "#{HASHES.NGINX_AUTH_PROXY_IMAGE_HASH}"
+            },
+            {
+              name  = "TF_VAR_ui_deploy_tag"
+              value = "#{HASHES.UI_IMAGE_HASH}"
+            },
+            {
+              name  = "INFRA_MODULES_COMMIT_HASH"
+              value = "#{HASHES.INFRA_MODULES_COMMIT_HASH}"
+            },
+            {
+              name  = "TF_VAR_conductor_deploy_tag"
+              value = "#{HASHES.CONDUCTOR_IMAGE_HASH}"
+            },
+            {
+              name  = "TF_VAR_core_worker_deploy_tag"
+              value = "#{HASHES.CORE_WORKER_IMAGE_HASH}"
+            },
+            {
+              name  = "TF_VAR_api_deploy_tag"
+              value = "#{HASHES.API_IMAGE_HASH}"
+            }
+          ]
+        )
+      }
+
+      input_artifacts = [
+        "infrastructure",
+        "application"
+      ]
+    }
+
+    action {
+      category  = "Build"
+      name      = "deploy-conductor-definitions"
+      owner     = "AWS"
+      provider  = "CodeBuild"
+      version   = "1"
+      run_order = 2
+
+      configuration = {
+        ProjectName   = module.deploy_e2e_test_conductor_definitions.pipeline_name
+        PrimarySource = "infrastructure"
+      }
+
+      input_artifacts = [
+        "infrastructure",
+        "core"
+      ]
+    }
+
+    action {
+      category  = "Build"
+      name      = "verify-leds-test-ecs-services"
+      owner     = "AWS"
+      provider  = "CodeBuild"
+      version   = "1"
+      run_order = 3
+
+      configuration = {
+        ProjectName   = module.verify_ecs_tasks.pipeline_name
+        PrimarySource = "infrastructure"
+
+        EnvironmentVariables = jsonencode(
+          [
+            {
+              name  = "ENVIRONMENT"
+              value = "e2e-test"
+            },
+            {
+              name  = "ASSUME_ROLE_ARN"
+              value = data.terraform_remote_state.shared_infra.outputs.integration_next_ci_arn
+            }
+          ]
+        )
+      }
+
+      input_artifacts = [
+        "infrastructure"
+      ]
+    }
+  }
+
+  stage {
     name = "deploy-and-test-preprod-environment"
     action {
       category  = "Build"
