8aab8a1302e4df96bfc77e9c2a1b5ee48fcba552
joe-fol <joefolkard@protonmail.com>
2025-03-10 13:10:18 +0000

    Give leds environment iam policy for codestar

--- terraform/shared_account_pathtolive_infra_ci/promotion_pipeline_path_to_live_iam.tf
+++ terraform/shared_account_pathtolive_infra_ci/promotion_pipeline_path_to_live_iam.tf
@@ -198,3 +198,33 @@ resource "aws_iam_role_policy" "remove_dev_sg_from_uat" {
   policy = local.allow_codebuild_codestar_connection_policy
   role   = module.remove_dev_sg_from_uat.pipeline_service_role_name
 }
+
+resource "aws_iam_role_policy" "deploy_leds_terraform" {
+  name   = "allow-codestar-connection"
+  policy = local.allow_codebuild_codestar_connection_policy
+  role   = module.deploy_leds_test_environment_terraform.pipeline_service_role_name
+}
+
+resource "aws_iam_role_policy" "run_leds_test_migrations" {
+  name   = "allow-codestar-connection"
+  policy = local.allow_codebuild_codestar_connection_policy
+  role   = module.run_leds_test_migrations.pipeline_service_role_name
+}
+
+resource "aws_iam_role_policy" "deploy_leds_conductor_definitions" {
+  name   = "allow-codestar-connection"
+  policy = local.allow_codebuild_codestar_connection_policy
+  role   = module.deploy_leds_conductor_definitions.pipeline_service_role_name
+}
+
+resource "aws_iam_role_policy" "apply_dev_sg_to_leds" {
+  name   = "allow-codestar-connection"
+  policy = local.allow_codebuild_codestar_connection_policy
+  role   = module.apply_dev_sg_to_uat.pipeline_service_role_name
+}
+
+resource "aws_iam_role_policy" "remove_dev_sg_from_leds" {
+  name   = "allow-codestar-connection"
+  policy = local.allow_codebuild_codestar_connection_policy
+  role   = module.remove_dev_sg_from_uat.pipeline_service_role_name
+}
