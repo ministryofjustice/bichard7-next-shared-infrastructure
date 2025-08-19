66f535d2b5eb50d7001845e578d1b2420ecf8665
joe-fol <joefolkard@protonmail.com>
2025-08-19 09:19:28 +0100

    Use amazon linux 2023 for our codebuild jobs

--- terraform/modules/codebuild_job/variables.tf
+++ terraform/modules/codebuild_job/variables.tf
@@ -76,7 +76,7 @@ variable "build_environments" {
   default = [
     {
       compute_type    = "BUILD_GENERAL1_SMALL"
-      image           = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+      image           = "aws/codebuild/amazonlinux-x86_64-standard:5.0"
       type            = "LINUX_CONTAINER"
       privileged_mode = true
     }
