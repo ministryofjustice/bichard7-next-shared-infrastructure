e50e98b0cc128ca7f07d7ef7862176840c580bbc
Tom Watkins <tom.watkins@justice.gov.uk>
2025-09-30 14:24:23 +0100

    Delete module

--- terraform/modules/shared_cd_common_jobs/outputs.tf
+++ terraform/modules/shared_cd_common_jobs/outputs.tf
@@ -8,16 +8,6 @@ output "build_bichard7_application_service_role_name" {
   value       = module.build_bichard7_next_docker_image.pipeline_service_role_name
 }
 
-output "build_user_service_name" {
-  description = "The name of our user-service docker image build job"
-  value       = module.build_bichard7_user_service_docker_image.pipeline_name
-}
-
-output "build_user_service_service_role_name" {
-  description = "The name of our build user service job service role"
-  value       = module.build_bichard7_user_service_docker_image.pipeline_service_role_name
-}
-
 output "build_audit_logging_name" {
   description = "The name of our audit logging docker image build job"
   value       = module.build_audit_logging.pipeline_name
