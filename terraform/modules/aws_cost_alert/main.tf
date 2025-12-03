96ca738fa1f35b4a71ecfa141d34d0b998642264
joe-fol <joefolkard@protonmail.com>
2025-12-03 12:42:37 +0000

    Create module for cloudwatch cost alerting

---
+++ terraform/modules/aws_cost_alert/main.tf
@@ -0,0 +1,19 @@
+resource "aws_sns_topic" "sandbox_cost_topic" {
+  name = var.sns_topic_name
+}
+
+resource "aws_cloudwatch_metric_alarm" "sandbox_cost_alert" {
+  alarm_name          = "${var.account_prefix}-cost-alert"
+  alarm_description   = "Billing alarm when EstimatedCharges exceed threshold"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  threshold           = var.threshold_usd
+
+  metric_name = "EstimatedCharges"
+  namespace   = "AWS/Billing"
+  statistic   = "Maximum"
+  period      = var.period_seconds
+
+  alarm_actions      = [aws_sns_topic.sandbox_cost_topic.arn]
+  treat_missing_data = "notBreaching"
+}
