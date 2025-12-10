e1e1c25bb1f8d23de4e5919c9fa1651fdf7a4ce2
padlichan <lili.berenyi@gmail.com>
2025-12-10 16:26:21 +0000

    Add terraform config to trigger deployment reminder codebuild job on schedule

--- terraform/shared_account_pathtolive_infra_ci/variables.tf
+++ terraform/shared_account_pathtolive_infra_ci/variables.tf
@@ -9,3 +9,29 @@ variable "is_cd" {
   type        = bool
   default     = false
 }
+
+variable "deployment_reminder_limit" {
+  description = "Deplyment overdue limit in days"
+  type = string
+  default = "0"
+}
+
+# variable "codebuild_s3_bucket" {
+#   description = "The name of our codebuild bucket used for logs and artificacts"
+#   type        = string
+# }
+
+# variable "sns_notifications_arn" {
+#   description = "The notifications arn for SNS"
+#   type        = string
+# }
+
+# variable "tags" {
+#   description = "A map of tags for our infrastructure"
+#   type        = map(string)
+# }
+
+# variable "notifications_kms_key_arn" {
+#   description = "The arn for our notifications KMS key"
+#   type        = string
+# }
