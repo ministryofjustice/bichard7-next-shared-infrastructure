bcd2baa159af22fb2ab7eb355e936f18f9f4547b
padlichan <lili.berenyi@gmail.com>
2025-12-15 10:37:11 +0000

    Remove comments

--- terraform/shared_account_pathtolive_infra_ci/variables.tf
+++ terraform/shared_account_pathtolive_infra_ci/variables.tf
@@ -15,23 +15,3 @@ variable "deployment_reminder_limit" {
   type        = string
   default     = "0"
 }
-
-# variable "codebuild_s3_bucket" {
-#   description = "The name of our codebuild bucket used for logs and artificacts"
-#   type        = string
-# }
-
-# variable "sns_notifications_arn" {
-#   description = "The notifications arn for SNS"
-#   type        = string
-# }
-
-# variable "tags" {
-#   description = "A map of tags for our infrastructure"
-#   type        = map(string)
-# }
-
-# variable "notifications_kms_key_arn" {
-#   description = "The arn for our notifications KMS key"
-#   type        = string
-# }
