1ee40651062d99ba6608cd3a0218ee7c898b343c
joe-fol <joefolkard@protonmail.com>
2026-03-03 12:06:18 +0000

    Allow sandbox to access logs to test

--- terraform/modules/shared_account_parent/csoc.tf
+++ terraform/modules/shared_account_parent/csoc.tf
@@ -4,55 +4,54 @@ resource "aws_kms_key" "csoc_sqs_key" {
   enable_key_rotation     = true
 
   policy = jsonencode({
-    Version : "2012-10-17",
-    Statement : [
+    Version = "2012-10-17",
+    Statement = [
       {
-        Sid : "Allow SQS to encrypt messages",
-        Effect : "Allow",
-        Principal : {
-          Service : "sqs.amazonaws.com",
-          AWS : "${data.aws_caller_identity.current.arn}"
+        Sid    = "Allow SQS to encrypt messages",
+        Effect = "Allow",
+        Principal = {
+          Service = "sqs.amazonaws.com",
+          AWS     = data.aws_caller_identity.current.arn
         },
-        Action : [
+        Action = [
           "kms:GenerateDataKey*",
           "kms:Decrypt"
         ],
-        "Resource" : "*"
+        Resource = "*"
       },
       {
-        Sid : "Enable IAM User Permissions",
-        Effect : "Allow",
-        Principal : {
-          AWS : [
-            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
-          ]
+        Sid    = "Enable IAM User Permissions",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
         },
-        Action : "kms:*",
-        Resource : "*"
+        Action   = "kms:*",
+        Resource = "*"
       },
       {
-        Sid : "Allow S3 to work with key",
-        Effect : "Allow",
-        Principal : {
-          Service : "s3.amazonaws.com"
+        Sid    = "Allow S3 to work with key",
+        Effect = "Allow",
+        Principal = {
+          Service = "s3.amazonaws.com"
         },
-        Action : [
+        Action = [
           "kms:GenerateDataKey",
           "kms:Decrypt"
         ],
-        Resource : "*"
+        Resource = "*"
       },
       {
-        Sid : "Allow cloudwatch to work with key",
-        Effect : "Allow",
-        Principal : {
-          Service : "events.amazonaws.com"
+        Sid    = "Allow cloudwatch to work with key",
+        Effect = "Allow",
+        Principal = {
+          Service = "events.amazonaws.com"
         },
-        Action : [
+        Action = [
           "kms:GenerateDataKey",
           "kms:Decrypt"
         ],
-        Resource : "*"
+        Resource = "*"
+      },
       {
         Sid    = "Allow CSOC to decrypt messages",
         Effect = "Allow",
@@ -112,18 +111,18 @@ resource "aws_iam_role_policy" "csoc_sqs_access" {
   role = aws_iam_role.csoc_role.id
 
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
+        Sid    = "AllowAccessToCsocMessageQueue",
+        Effect = "Allow",
         Action = [
           "sqs:ReceiveMessage",
           "sqs:DeleteMessage",
           "sqs:GetQueueAttributes"
-        ]
-        Effect   = "Allow"
+        ],
         Resource = "arn:aws:sqs:eu-west-2:497078235711:csoc-queue"
       }
     ]
   })
 }
-
