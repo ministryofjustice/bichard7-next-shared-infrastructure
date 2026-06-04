d221df6cd5a95412368e61d2fe3a3309a94c2356
Tom Watkins <tom.watkins@justice.gov.uk>
2026-06-04 11:41:29 +0100

    Use node 24 repo in reporting and audit logging artifacts

--- terraform/modules/shared_cd_common_jobs/data.tf
+++ terraform/modules/shared_cd_common_jobs/data.tf
@@ -4,15 +4,14 @@ data "aws_region" "current_region" {
 data "aws_caller_identity" "current" {
 }
 
-
-data "aws_ecr_repository" "nodejs_20_2023" {
-  name = "nodejs-20-2023"
+data "aws_ecr_repository" "nodejs_24_2023" {
+  name = "nodejs-24-2023"
 }
 
-data "external" "latest_nodejs_20_2023_image" {
+data "external" "latest_nodejs_24_2023_image" {
   program = [
     "aws", "ecr", "describe-images",
-    "--repository-name", data.aws_ecr_repository.nodejs_20_2023.name,
+    "--repository-name", data.aws_ecr_repository.nodejs_24_2023.name,
     "--query", "{\"tags\": to_string(sort_by(imageDetails,& imagePushedAt)[-1].imageDigest)}",
   ]
 }
