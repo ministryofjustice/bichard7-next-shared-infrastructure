1e3720b2f271d0155419d4b2042ededca9890671
Tausif Patel <tausifpatel.dev@gmail.com>
2026-06-10 13:17:43 +0100

    Add a resource(niam slack webhook ssm parameter)

---
+++ terraform/shared_account_pathtolive_infra_ci/niam_expiry_alert.tf
@@ -0,0 +1,16 @@
+## SSM PARAM
+resource "aws_ssm_parameter" "niam_slack_webhook" {
+  name = "/monitoring/slack/niam_webhook"
+  type = "SecureString"
+  value = "-"
+
+  tags = module.label.tags
+
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
+}
+
+## Codebuild - pass SSM path as env var
