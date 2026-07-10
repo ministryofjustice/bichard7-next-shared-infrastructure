bb0f61d33d5cfcb433be67d38669e92d5b56f7be
Kate Reidy <kate.reidy@madetech.com>
2026-07-10 16:13:03 +0100

    Remove references to sandbox c admin role
    Co-Authored-By: Joe Folkard <joe.folkard@madetech.com>
    Co-Authored-By: Caitlin Barnard <caitlin@madetech.com>

--- scripts/set-terraform-access-keys.sh
+++ scripts/set-terraform-access-keys.sh
@@ -13,7 +13,6 @@ ACCOUNTS=$(aws ssm get-parameter --name $ACCOUNTS_SSM_KEY --query "Parameter.Val
 if [[ "$ENVIRONMENT" == "sandbox" ]]; then
   SANDBOX_A_ADMIN_ROLE=$(echo $ACCOUNTS | jq -r ".sandbox_a")
   SANDBOX_B_ADMIN_ROLE=$(echo $ACCOUNTS | jq -r ".sandbox_b")
-  SANDBOX_C_ADMIN_ROLE=$(echo $ACCOUNTS | jq -r ".sandbox_c")
 
   SANDBOX_A=$(aws sts assume-role --role-arn $SANDBOX_A_ADMIN_ROLE --role-session-name sandbox-a)
   export TF_VAR_sandbox_a_access_key=$(echo "${SANDBOX_A}" | jq -r ".Credentials.AccessKeyId")
@@ -24,11 +23,6 @@ if [[ "$ENVIRONMENT" == "sandbox" ]]; then
   export TF_VAR_sandbox_b_access_key=$(echo "${SANDBOX_B}" | jq -r ".Credentials.AccessKeyId")
   export TF_VAR_sandbox_b_secret_key=$(echo "${SANDBOX_B}" | jq -r ".Credentials.SecretAccessKey")
   export TF_VAR_sandbox_b_session_token=$(echo "${SANDBOX_B}" | jq -r ".Credentials.SessionToken")
-
-  SANDBOX_C=$(aws sts assume-role --role-arn $SANDBOX_C_ADMIN_ROLE --role-session-name sandbox-c)
-  export TF_VAR_sandbox_c_access_key=$(echo "${SANDBOX_C}" | jq -r ".Credentials.AccessKeyId")
-  export TF_VAR_sandbox_c_secret_key=$(echo "${SANDBOX_C}" | jq -r ".Credentials.SecretAccessKey")
-  export TF_VAR_sandbox_c_session_token=$(echo "${SANDBOX_C}" | jq -r ".Credentials.SessionToken")
 
 elif [[ "$ENVIRONMENT" == "pathtolive" ]]; then
   INTEGRATION_NEXT_ADMIN_ROLE=$(echo $ACCOUNTS | jq -r ".integration_next")
