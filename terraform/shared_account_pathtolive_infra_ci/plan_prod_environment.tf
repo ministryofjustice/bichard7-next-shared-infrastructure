f1ccfcbcecfca03bfe7ffaf13861f3a69d8c8ae7
Kate Reidy <522347+katerd@users.noreply.github.com>
2026-07-22 15:05:09 +0100

    Add production plan codebuild (#439)
    * Add terraform plan codebuild job
    
    * Use production for AWS_ACCOUNT_NAME
    
    * Add PNC ip - if it's not specified the PNC emulator will be deployed in its place
    
    * Add missing required use_smtp_service terraform variable so plan can match apply
    
    ---------
    
    Co-authored-by: joe folkard <joe.folkard@madetech.com>

---
+++ terraform/shared_account_pathtolive_infra_ci/plan_prod_environment.tf
@@ -0,0 +1,114 @@
+module "plan_prod_terraform" {
+  source = "../modules/codebuild_job"
+
+  build_description      = "Codebuild for running a terraform plan for the prod infrastructure"
+  codepipeline_s3_bucket = module.codebuild_base_resources.codepipeline_bucket
+  name                   = "plan-prod-environment"
+  repository_name        = "bichard7-next-infrastructure"
+  buildspec_file         = "buildspecs/plan-terraform.yml"
+  sns_kms_key_arn        = module.codebuild_base_resources.notifications_kms_key_arn
+  sns_notification_arn   = module.codebuild_base_resources.notifications_arn
+  vpc_config             = module.codebuild_base_resources.codebuild_vpc_config_blocks["production"]
+
+  build_timeout = 180
+
+  build_environments = local.codebuild_2023_pipeline_build_environments
+
+  codebuild_secondary_sources = [
+    {
+      type              = "GITHUB"
+      location          = "https://github.com/ministryofjustice/bichard7-next.git"
+      git_clone_depth   = 1
+      source_identifier = "bichard7_next"
+      git_submodules_config = {
+        fetch_submodules = true
+      }
+    }
+  ]
+
+  deploy_account_name = "production"
+  deployment_name     = "production"
+  event_type_ids      = []
+
+  allowed_resource_arns = [
+    module.codebuild_docker_resources.liquibase_repository_arn,
+    module.codebuild_docker_resources.amazon_linux_2_repository_arn,
+    data.aws_ecr_repository.bichard.arn,
+    data.aws_ecr_repository.codebuild_base.arn,
+    module.codebuild_docker_resources.codebuild_2023_base.arn
+  ]
+
+  environment_variables = [
+    {
+      name  = "DEPLOY_ENV"
+      value = "pathtolive"
+    },
+    {
+      name  = "WORKSPACE"
+      value = "production"
+    },
+    {
+      name  = "USER_TYPE"
+      value = "ci"
+    },
+    {
+      name  = "AWS_ACCOUNT_NAME"
+      value = "production"
+    },
+    {
+      name  = "AUTO_APPROVE"
+      value = false
+    },
+    {
+      name  = "TF_VAR_override_deploy_tags"
+      value = "true"
+    },
+    {
+      name  = "ASSUME_ROLE_ARN"
+      value = data.terraform_remote_state.shared_infra.outputs.production_ci_arn
+    },
+    {
+      name  = "LIQUIBASE_IMAGE"
+      value = local.latest_liquibase_image
+    },
+    {
+      name  = "PARENT_ACCOUNT_ID"
+      value = data.aws_caller_identity.current.account_id
+    },
+    {
+      name  = "USE_PEERING"
+      value = "true"
+    },
+    {
+      name  = "USE_SMTP"
+      value = "true"
+    },
+    {
+      name  = "ARTIFACT_BUCKET"
+      value = module.codebuild_base_resources.codepipeline_bucket
+    },
+    {
+      name  = "TF_VAR_is_production"
+      value = true
+    },
+    {
+      name  = "TF_VAR_pnc_ip"
+      value = "172.31.4.204"
+    },
+    {
+      name  = "TF_VAR_aurora_db_version"
+      value = "15.15"
+    },
+    {
+      name  = "TF_VAR_bichard_deploy_tag"
+      value = "unassigned_as_plan" # Hardcode so the plan doesn't fail due to blank SSM params
+    },
+    {
+      name  = "TF_VAR_use_smtp_service"
+      value = "true"
+    },
+  ]
+  is_cd = true
+
+  tags = module.label.tags
+}
