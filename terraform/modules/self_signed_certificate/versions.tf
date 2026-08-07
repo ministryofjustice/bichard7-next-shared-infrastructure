456fd02d5a25bd14672a361ed243855f5b869b02
Kate Reidy <kate.reidy@madetech.com>
2026-08-07 15:04:26 +0100

    Upgrade to aws provider 6.58.0

--- terraform/modules/self_signed_certificate/versions.tf
+++ terraform/modules/self_signed_certificate/versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "6.58.0"
     }
     tls = {
       source  = "hashicorp/tls"
--- terraform/modules/shared_cd_common_jobs/versions.tf
+++ terraform/modules/shared_cd_common_jobs/versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "6.58.0"
     }
   }
   required_version = ">= 0.13"
