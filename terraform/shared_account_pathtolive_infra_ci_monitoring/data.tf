316272ff8dc3f2a11569f27d7d22ed4c0a24bf2d
joe folkard <joe.folkard@madetech.com>
2026-09-24 11:40:10 +0100

    Remove dynamoDB from state lock config

--- terraform/shared_account_pathtolive_infra_ci_monitoring/data.tf
+++ terraform/shared_account_pathtolive_infra_ci_monitoring/data.tf
@@ -7,22 +7,20 @@ data "aws_caller_identity" "current" {
 data "terraform_remote_state" "shared_infra" {
   backend = "s3"
   config = {
-    bucket         = local.remote_bucket_name
-    dynamodb_table = "${local.remote_bucket_name}-lock"
-    use_lockfile   = true
-    key            = "tfstatefile"
-    region         = "eu-west-2"
+    bucket       = local.remote_bucket_name
+    use_lockfile = true
+    key          = "tfstatefile"
+    region       = "eu-west-2"
   }
 }
 
 data "terraform_remote_state" "shared_infra_ci" {
   backend = "s3"
   config = {
-    bucket         = local.remote_bucket_name
-    dynamodb_table = "${local.remote_bucket_name}-lock"
-    use_lockfile   = true
-    key            = "ci/tfstatefile"
-    region         = "eu-west-2"
+    bucket       = local.remote_bucket_name
+    use_lockfile = true
+    key          = "ci/tfstatefile"
+    region       = "eu-west-2"
   }
 }
 
